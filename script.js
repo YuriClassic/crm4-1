c66d41b5e5878f6b471e42d88181efb03edc8bc3
“Yuri <yu_23bsp@mail.ru>
2025-02-18 21:46:26 +0500

    yes

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 'use strict';
 
 const productName = prompt("Наименование товара");
-const productQuantity = prompt("Количество товара");
+const productQuantity = Number(prompt("Количество товара")) || 0;
 const productCategory = prompt("Категория товара");
-const productPrice = prompt("Цена товара");
+const productPrice = Number(prompt("Цена товара")) || 0;
 
 console.log("Тип данных 'Количество товара':", typeof productQuantity);
 console.log("Тип данных 'Цена товара':", typeof productPrice);
 
-const quantity = Number(productQuantity);
-const price = Number(productPrice);
-
-const isValidQuantity = !isNaN(quantity);
-const isValidPrice = !isNaN(price);
-
-const finalQuantity = isValidQuantity ? quantity : 0; 
-const finalPrice = isValidPrice ? price : 0;         
-
-console.log("Тип данных 'Количество товара' после преобразования:", typeof finalQuantity);
-console.log("Тип данных 'Цена товара' после преобразования:", typeof finalPrice);
-
-const totalPrice = finalQuantity * finalPrice;
-console.log(`Вы заказали ${finalQuantity} ед. товара "${productName}" на сумму ${totalPrice} у.е.`);
+const totalPrice = productQuantity * productPrice;
+console.log(`Вы заказали ${productQuantity} ед. товара "${productName}" на сумму ${totalPrice} у.е.`);
