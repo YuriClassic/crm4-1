f5ad284a5924d6edb3abc05bf56a7df6b7c54738
“Yuri <yu_23bsp@mail.ru>
2025-02-18 20:47:46 +0500

    4-2

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const productName = "rane one mk2"; 
-const productQuantity = 10;
-const productCategory = "DJ controllers";
-const productPrice = 1500;
+const productName = prompt("Наименование товара");
+const productQuantity = prompt("Количество товара");
+const productCategory = prompt("Категория товара");
+const productPrice = prompt("Цена товара");
 
-console.log(productName);
+console.log("Тип данных 'Количество товара':", typeof productQuantity);
+console.log("Тип данных 'Цена товара':", typeof productPrice);
+
+const quantity = Number(productQuantity);
+const price = Number(productPrice);
+
+const isValidQuantity = !isNaN(quantity);
+const isValidPrice = !isNaN(price);
+
+const finalQuantity = isValidQuantity ? quantity : 0; 
+const finalPrice = isValidPrice ? price : 0;         
+
+console.log("Тип данных 'Количество товара' после преобразования:", typeof finalQuantity);
+console.log("Тип данных 'Цена товара' после преобразования:", typeof finalPrice);
+
+const totalPrice = finalQuantity * finalPrice;
+console.log(`Вы заказали ${finalQuantity} ед. товара "${productName}" на сумму ${totalPrice} у.е.`);
